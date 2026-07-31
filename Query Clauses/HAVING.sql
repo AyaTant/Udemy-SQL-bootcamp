ddd39ade15a6a936ecb86845835581e9ce21eeb2
Aya Tantaoui <ayatantaoui12@gmail.com>
2026-07-31 12:22:56 +0100

    having clauses examples

---
+++ Query Clauses/HAVING.sql
@@ -0,0 +1,15 @@
+/* find the average score for each country considering onlu customers with a score not equal to 0
+   and return only those countries with an average score greater than 430
+*/
+
+SELECT 
+	   country,
+	   AVG(score) as avg_score
+FROM customers
+WHERE score != 0
+GROUP BY country
+HAVING AVG (score) > 430
+
+-- return unique list of all countries
+SELECT DISTINCT country
+from customers
