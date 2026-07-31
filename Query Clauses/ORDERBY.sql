aab25356b5fb0aa2cb34060ed5b479d92ab04e1d
Aya Tantaoui <ayatantaoui12@gmail.com>
2026-07-31 12:23:07 +0100

    order by clauses examples

---
+++ Query Clauses/ORDERBY.sql
@@ -0,0 +1,17 @@
+/* Retrieve all customers and sort 
+the results by the highest score first
+*/
+SELECT *
+FROM customers
+ORDER BY score DESC
+
+-- sort by lowest
+SELECT *
+FROM customers
+ORDER BY score ASC
+
+-- Retrieve all customers and sort the results by the country and then by the highest score
+SELECT *
+FROM customers
+ORDER BY country ASC, score DESC
+
